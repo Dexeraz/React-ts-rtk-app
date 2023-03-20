ebc71de924fb5d59919e3dd3f278d76eed78e0ae
MBilik <dexeraz@gmail.com>
2023-03-20 20:33:19 +0100

    implemented customer card for food notes. customerSlice created and implemented. Logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import { addReservation } from "./features/reservationSlice";
 import { RootState } from "./app/store";
 import ReservationCard from "./components/ReservationCard";
-import { addReservation } from "./features/reservationSlice";
+import CustomerCard from "./components/CustomerCard";
 
 function App() {
-  const [reservationName, setReservationName] = useState("");
+  const dispatch = useDispatch();
+
+  const [reservationNameInput, setReservationNameInput] = useState("");
 
   const reservations = useSelector(
     (state: RootState) => state.reservations.value
   );
-
-  const dispatch = useDispatch();
-
+  const customers = useSelector((state: RootState) => state.customer.value);
   const handleAddReservations = () => {
-    if (!reservationName) return;
-    dispatch(addReservation(reservationName));
-    setReservationName("");
+    dispatch(addReservation(reservationNameInput));
   };
 
   return (
@@ -27,34 +27,32 @@ function App() {
           <div>
             <h5 className="reservation-header">Reservations</h5>
             <div className="reservation-cards-container">
-              <div className="reservation-cards-container">
-                {reservations.map((name: string, index: number) => {
-                  return <ReservationCard name={name} index={index}/>;
-                })}
-              </div>
+              {reservations.map((name, index) => {
+                return <ReservationCard name={name} index={index} />;
+              })}
             </div>
-            <div className="reservation-input-container">
-              <input
-                value={reservationName}
-                onChange={(e) => {
-                  setReservationName(e.target.value);
-                }}
+          </div>
+          <div className="reservation-input-container">
+            <input
+              value={reservationNameInput}
+              onChange={(e) => {
+                setReservationNameInput(e.target.value);
+              }}
+            />
+            <button onClick={handleAddReservations}>Add</button>
+          </div>
+        </div>
+        <div className="customer-food-container">
+          {customers.map((customer) => {
+            return (
+              <CustomerCard
+                id={customer.id}
+                name={customer.name}
+                food={customer.food}
+                key={customer.id}
               />
-              <button onClick={handleAddReservations}>Add</button>
-            </div>
-          </div>
-          <div className="customer-food-container">
-            <div className="customer-food-card-container">
-              <p>Selena Gomez</p>
-              <div className="customer-foods-container">
-                <div className="customer-food"></div>
-                <div className="customer-food-input-container">
-                  <input />
-                  <button>Add</button>
-                </div>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
